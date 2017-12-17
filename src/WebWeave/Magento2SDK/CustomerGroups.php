f2211894232e7bf86b1462fc991e2f3aee37c639
Sander de Jonge <sander@web-weave.com>
2017-12-17 10:25:49 +0100

    delete and update

--- src/WebWeave/Magento2SDK/CustomerGroups.php
+++ src/WebWeave/Magento2SDK/CustomerGroups.php
@@ -9,6 +9,10 @@ class CustomerGroups
 	 */
 	protected $apiClient;
 
+	/**
+	 * CustomerGroups constructor.
+	 * @param ApiClient $apiClient
+	 */
 	public function __construct(\WebWeave\Magento2SDK\ApiClient $apiClient)
 	{
 		$this->apiClient = $apiClient;
@@ -45,6 +49,57 @@ class CustomerGroups
 		} catch (\Exception $exception) {
 			throw $exception;
 		}
-
 	}
+
+	/**
+	 * @param $id
+	 * @throws \Exception
+	 * @return boolean
+	 */
+	public function deleteCustomerGroup($id)
+	{
+		try {
+			$res = $this->apiClient->apiClient->delete('customerGroups/'.$id);
+
+			return json_decode($res->getBody()->getContents());
+		} catch (\Exception $exception) {
+			throw $exception;
+		}
+	}
+
+	/**
+	 * @param $id
+	 * @param array $data
+	 * @throws \Exception
+	 * @return array
+	 */
+	public function updateCustomerGroup($id, array $data)
+	{
+		try {
+			$res = $this->apiClient->apiClient->put('customerGroups/'.$id, ['body' => json_encode($data)]);
+
+			return json_decode($res->getBody()->getContents());
+		} catch (\Exception $exception) {
+			throw $exception;
+		}
+	}
+
+	/**
+	 * @param array $data
+	 * @return mixed
+	 * @throws \Exception
+	 */
+	public function searchCustomerGroup(array $data)
+	{
+		//TODO Create search method
+		return "Not ready yet";
+		try {
+			$res = $this->apiClient->apiClient->get('customerGroups/search');
+
+			return json_decode($res->getBody()->getContents());
+		} catch (\Exception $exception) {
+			throw $exception;
+		}
+	}
+
 }
