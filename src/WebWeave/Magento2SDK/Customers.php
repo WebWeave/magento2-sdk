c3297130695689b37c0c215db6c17e49a913a2c4
Sander de Jonge <sander@web-weave.com>
2017-12-18 12:36:43 +0100

    Customers

--- src/WebWeave/Magento2SDK/Customers.php
+++ src/WebWeave/Magento2SDK/Customers.php
@@ -50,4 +50,55 @@ class Customers
 		}
 	}
 
+	/**
+	 * @param $id
+	 * @return boolean
+	 * @throws \Exception
+	 */
+	public function deleteCustomer($id)
+	{
+		try {
+			$res = $this->apiClient->apiClient->delete('customers/'.$id);
+
+			return json_decode($res->getBody()->getContents());
+		} catch (\Exception $exception) {
+			throw $exception;
+		}
+	}
+
+	/**
+	 * @param $id
+	 * @param array $data
+	 * @return array
+	 * @throws \Exception
+	 */
+	public function updateCustomer($id, array $data)
+	{
+		try {
+			$res = $this->apiClient->apiClient->put('customers/'.$id, ['body' => json_encode($data)]);
+
+			return json_decode($res->getBody()->getContents());
+		} catch (\Exception $exception) {
+			throw $exception;
+		}
+	}
+
+	/**
+	 * @param array $data
+	 * @return mixed
+	 * @throws \Exception
+	 */
+	public function searchCustomers(array $data)
+	{
+		//TODO Create search method
+		return "Not ready yet";
+		try {
+			$res = $this->apiClient->apiClient->get('customers/search');
+
+			return json_decode($res->getBody()->getContents());
+		} catch (\Exception $exception) {
+			throw $exception;
+		}
+	}
+
 }
